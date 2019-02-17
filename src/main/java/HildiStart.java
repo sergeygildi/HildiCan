7dc1d3e901b6211146866b7a8fb5bcfe8305ab91
sergeygildi <sergeygildi@gmail.com>
2019-02-18 00:10:33 +0200

    1.Rebase project for maven default 'main/java' dir. 2.Refactoring code. Now it work great with arguments too. 3. Clear pom.xml file.

--- src/main/java/HildiStart.java
+++ src/main/java/HildiStart.java
@@ -1,10 +1,11 @@
-import org.apache.commons.lang3.ArrayUtils;
+import com.sun.xml.internal.xsom.impl.scd.Step;
 
 import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 
 public class HildiStart {
 
@@ -18,7 +19,7 @@ public class HildiStart {
 
     public static void main(String[] args) {
 
-        if(!ArrayUtils.isNotEmpty(args) && args.length == 0) {
+        if(args.length == 0) {
             getFileList();
         } else {
             getFileList(args);
@@ -30,15 +31,15 @@ public class HildiStart {
         printAllFilesAtDir(dir);
     }
 
-    private static void getFileList(String[] args){
-        dir = Paths.get("./" + args).normalize();
+    private static void getFileList(String[] path){
+        dir = Paths.get("./" + path[0]).getFileName().normalize();
         printAllFilesAtDir(dir);
     }
 
     private static void printAllFilesAtDir(Path dir) {
         try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir, "*")) {
             for (Path file : stream) {
-                System.out.print(file + " ");
+                System.out.println(file + " ");
             }
         } catch(IOException e1) {
             System.out.println("Carl, we have a problem!");
