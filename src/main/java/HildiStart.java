6de16c20e508c13cd892158cf5a6a02f1e9ea6df
Serhii Gildi <14317559+sergeygildi@users.noreply.github.com>
2019-02-27 18:27:26 +0200

    Update HildiStart.java

--- src/main/java/HildiStart.java
+++ src/main/java/HildiStart.java
@@ -4,15 +4,6 @@ import java.nio.file.Paths;
 
 public class HildiStart {
 
-    /**
-     * Зарефакторить код                                                                done
-     * скрыть из дериктории все системные файлы. Показать только при флаге "-а"         done
-     * Обработать корректно эксепшн                                                     done/delete
-     * Абсолюнтые/относительные пути   dir.file.listfiles                               done
-     * Манифест (мавен)                                                                 done
-     * Сделать корректный вывод как в "ls"
-     */
-
     public static void main(String[] args) {
 
         Path target, targetPath;
