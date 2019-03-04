2333602a0d9bd6458f923592c40f3e19e689bdcc
Serhii Gildi <14317559+sergeygildi@users.noreply.github.com>
2019-03-04 15:48:00 +0200

    Update HildiStart.java
    Updates code, refactoring some methods. Write javaDocs.

--- src/main/java/HildiStart.java
+++ src/main/java/HildiStart.java
@@ -4,35 +4,61 @@ import java.nio.file.Paths;
 
 public class HildiStart {
 
+    private static Path target;
+
     public static void main(String[] args) {
 
-        Path target, targetPath;
+        Path targetPath;
 
-        if (args.length != 0) {
-            targetPath = Paths.get(args[0]);
-            if (targetPath.isAbsolute()) {
-                target = targetPath;
+        if (args.length > 0) {
+
+            if (args[0].equals("-a")) {
+
+                /** When argument with a flag. */
+                if (args.length == 2) {
+                    targetPath = Paths.get(args[1]);
+                    Path path = checkDirectory(targetPath);
+                    printAllFilesAtDir(path, true);
+                } else {
+                /** When we don't have second arg. Only flag. */
+                    printAllFilesAtDir(getCurrentDirectory(), true);
+                }
+
             } else {
-                target = getCurrentDirectory().resolve(targetPath);
+                /** When argument without flag. */
+                targetPath = Paths.get(args[0]);
+                Path path = checkDirectory(targetPath);
+                printAllFilesAtDir(path, false);
             }
+            /** Without arguments. */
         } else {
-            target = getCurrentDirectory();
+            printAllFilesAtDir(getCurrentDirectory(), false);
         }
+    }
 
-        printAllFilesAtDir(target);
+    private static Path checkDirectory(Path targetPath) {
+        if (targetPath.isAbsolute()) {
+            return target = targetPath;
+        } else {
+            return target = getCurrentDirectory().resolve(targetPath);
+        }
     }
 
     private static Path getCurrentDirectory() {
         return Paths.get(System.getProperty("user.dir"));
     }
 
-    private static void printAllFilesAtDir(Path dir) {
+    private static void printAllFilesAtDir(Path dir, boolean showHiddenDir) {
 
         File[] stream = dir.toFile().listFiles();
 
         if (stream != null) {
             for (File file : stream) {
-                if (!file.isHidden()) {
+                if (!showHiddenDir) {
+                    if (!file.isHidden()){
+                        System.out.print(file.getName() + " ");
+                    }
+                } else {
                     System.out.print(file.getName() + " ");
                 }
             }
